5591c6b8d0a792e2c42e2f17d60b5d89fb2640e9
Jonathan Diego Rodríguez Rodríguez <jon_eldiablo@hotmail.com>
2019-07-16 16:41:54 -0500

    form selector

--- src/App.js
+++ src/App.js
@@ -1,39 +1,65 @@
 import React from 'react';
 import Form from "react-jsonschema-form";
-import { register } from "./schemas";
+import schemas from "./schemas";
 import './App.scss';
 
-
-
-const onSubmit = async (e) => {
-  let response = await fetch('api/form.json', {
-    method: 'POST',
-    body: JSON.stringify(e.formData),
-    headers: {
-      'Content-Type': 'application/json'
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      schema: schemas.register.schema,
+      uiSchema: schemas.register.uiSchema
     }
-  }).then(res => res.json())
-    .catch(error => {
-      console.error('Error:', error);
-      return false;
-    });
+    this.onChange = this.onChange.bind(this);
+  };
+  async onSubmit(e) {
+    let response = await fetch('api/form.json', {
+      method: 'POST',
+      body: JSON.stringify(e.formData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json())
+      .catch(error => {
+        console.error('Error:', error);
+        return false;
+      });
 
-  if (response) {
-    console.log(response);
+    if (response) {
+      console.log(response);
+    }
+
   }
 
-}
+  onChange(e) {
+    this.setState({
+      schema: schemas[e.target.value].schema,
+      uiSchema: schemas[e.target.value].uiSchema
+    })
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <div className="row justify-content-center">
-        <div className="col-10 col-sm-8 col-md-6 col-lg-4">
-          <Form schema={register.schema} uiSchema={register.uiSchema} onSubmit={onSubmit} />
+  render() {
+    return (
+      <div className="App">
+        <br />
+        <div className="row justify-content-center">
+          <div className="col-10 col-sm-8 col-md-6 col-lg-4">
+            <div className="form-group from-inline">
+              <label htmlFor="selectForm">Selecciona un formulario...</label>
+              <select onChange={this.onChange} id="selectForm" className="form-control">
+                <option disabled>Selecciona un esquema</option>
+                <option value="register">registro</option>
+                <option value="quest">cuestionario</option>
+              </select>
+            </div>
+            <hr />
+            <Form schema={this.state.schema} uiSchema={this.state.uiSchema} onSubmit={this.onSubmit} />
+          </div>
         </div>
+        <br />
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/schemas/index.js
+++ src/schemas/index.js
@@ -1,4 +1,7 @@
 import registerSchema from "./registerForm/schema.json";
 import registerUiSchema from "./registerForm/ui-schema.json";
-let register = { schema: registerSchema, uiSchema: registerUiSchema }
-export { register };
+import questSchema from "./questForm/schema.json";
+import questUiSchema from "./questForm/ui-schema.json";
+let register = { schema: registerSchema, uiSchema: registerUiSchema };
+let quest = { schema: questSchema, uiSchema: questUiSchema };
+export default { register, quest };
