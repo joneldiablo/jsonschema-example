1822e2ddc1cb02497447c36339cde5967d8cebcf
Jonathan Diego Rodríguez Rodríguez <jon_eldiablo@hotmail.com>
2019-07-16 15:48:15 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Form from "react-jsonschema-form";
+import './App.scss';
+
+const schema = {
+  title: "Formulario de registro",
+  type: "object",
+  properties: {
+    user: {
+      type: "object",
+      title: "datos de usuario",
+      required: ["firstname"],
+      properties: {
+        firstname: { type: "string", title: "Nombre", default: "" },
+        lastname: { type: "string", title: "Apellidos", default: "" }
+      }
+    },
+    address: {
+      type: "object",
+      title: "Dirección",
+      properties: {
+        zipcode: { type: "string", title: "Código postal", default: "" },
+        street: { type: "string", title: "Calle", default: "" },
+        city: { type: "string", title: "Ciudad", default: "" },
+        country: { type: "string", title: "Pais", default: "" }
+      }
+    }
+  }
+}
+
+const uiSchema = {
+  user: {
+    firstname: {
+      "ui:options": {
+        label: false,
+      },
+      "ui:placeholder": "Nombre*"
+    },
+    lastname: {
+      "ui:options": {
+        label: false,
+      },
+      "ui:placeholder": "Apellidos"
+    }
+  },
+  address: {
+    zipcode: {
+      "ui:options": { label: false },
+      "ui:placeholder": "Código postal"
+    },
+    street: {
+      "ui:options": { label: false },
+      "ui:placeholder": "Calle"
+    },
+    city: {
+      "ui:options": { label: false },
+      "ui:placeholder": "Ciudad"
+    },
+    country: {
+      "ui:options": { label: false },
+      "ui:placeholder": "Pais"
+    }
+  }
+
+}
+
+const onSubmit = async (e) => {
+  let response = await fetch('api/form.json', {
+    method: 'POST',
+    body: JSON.stringify(e.formData),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then(res => res.json())
+    .catch(error => {
+      console.error('Error:', error);
+      return false;
+    });
+
+  if (response) {
+    console.log(response);
+  }
+
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="row justify-content-center">
+        <div className="col-10 col-sm-8 col-md-6 col-lg-4">
+          <Form schema={schema} uiSchema={uiSchema} onSubmit={onSubmit} />
+        </div>
+      </div>
     </div>
   );
 }
