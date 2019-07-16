f20c4aa48a58011e664cb13dc2441ec235ed9ee8
Jonathan Diego Rodríguez Rodríguez <jon_eldiablo@hotmail.com>
2019-07-16 16:07:26 -0500

    estructura para colocar multiples schemas

--- src/App.js
+++ src/App.js
@@ -1,68 +1,9 @@
 import React from 'react';
 import Form from "react-jsonschema-form";
+import { register } from "./schemas";
 import './App.scss';
 
-const schema = {
-  title: "Formulario de registro",
-  type: "object",
-  properties: {
-    user: {
-      type: "object",
-      title: "datos de usuario",
-      required: ["firstname"],
-      properties: {
-        firstname: { type: "string", title: "Nombre", default: "" },
-        lastname: { type: "string", title: "Apellidos", default: "" }
-      }
-    },
-    address: {
-      type: "object",
-      title: "Dirección",
-      properties: {
-        zipcode: { type: "string", title: "Código postal", default: "" },
-        street: { type: "string", title: "Calle", default: "" },
-        city: { type: "string", title: "Ciudad", default: "" },
-        country: { type: "string", title: "Pais", default: "" }
-      }
-    }
-  }
-}
 
-const uiSchema = {
-  user: {
-    firstname: {
-      "ui:options": {
-        label: false,
-      },
-      "ui:placeholder": "Nombre*"
-    },
-    lastname: {
-      "ui:options": {
-        label: false,
-      },
-      "ui:placeholder": "Apellidos"
-    }
-  },
-  address: {
-    zipcode: {
-      "ui:options": { label: false },
-      "ui:placeholder": "Código postal"
-    },
-    street: {
-      "ui:options": { label: false },
-      "ui:placeholder": "Calle"
-    },
-    city: {
-      "ui:options": { label: false },
-      "ui:placeholder": "Ciudad"
-    },
-    country: {
-      "ui:options": { label: false },
-      "ui:placeholder": "Pais"
-    }
-  }
-
-}
 
 const onSubmit = async (e) => {
   let response = await fetch('api/form.json', {
@@ -88,7 +29,7 @@ function App() {
     <div className="App">
       <div className="row justify-content-center">
         <div className="col-10 col-sm-8 col-md-6 col-lg-4">
-          <Form schema={schema} uiSchema={uiSchema} onSubmit={onSubmit} />
+          <Form schema={register.schema} uiSchema={register.uiSchema} onSubmit={onSubmit} />
         </div>
       </div>
     </div>
---
+++ src/schemas/index.js
@@ -0,0 +1,4 @@
+import registerSchema from "./registerForm/schema.json";
+import registerUiSchema from "./registerForm/ui-schema.json";
+let register = { schema: registerSchema, uiSchema: registerUiSchema }
+export { register };
